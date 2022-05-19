4c37ec3e61d670136793fb798811cc0970ecb04b
Adam M Messina <91221002+amessina1999@users.noreply.github.com>
2022-05-19 11:45:12 -0400

    Update app.js
    Added Theme feature to toggle light and dark mode

--- app.js
+++ app.js
@@ -33,6 +33,12 @@ function PageTransitions(){
             element.classList.add('active');
         }
     })
+    //Toggle THeme
+    const themeBtn = document.querySelector('.theme-btn');
+    themeBtn.addEventListener('click', () =>{
+        let element = document.body;
+        element.classList.toggle('light-mode')
+    })
 }
 
     PageTransitions();
